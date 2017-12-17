f6ab26dac91f041ed88763c48099e90d87994ad5
joo <ozzznet@naver.com>
2017-12-17 13:56:06 +0900

    Release Web 0.0.4

--- src/main/java/org/ptlu/join/JoinApplication.java
+++ src/main/java/org/ptlu/join/JoinApplication.java
@@ -5,25 +5,25 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
-@SpringBootApplication
-public class JoinApplication extends SpringBootServletInitializer{
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(JoinApplication.class);
-	}
-
-	public static void main(String[] args) {
-		SpringApplication.run(JoinApplication.class, args);
-	}
-}
-
-
-
 //@SpringBootApplication
-//public class JoinApplication {
+//public class JoinApplication extends SpringBootServletInitializer{
+//
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//		return builder.sources(JoinApplication.class);
+//	}
 //
 //	public static void main(String[] args) {
 //		SpringApplication.run(JoinApplication.class, args);
 //	}
 //}
+
+
+
+@SpringBootApplication
+public class JoinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoinApplication.class, args);
+	}
+}
